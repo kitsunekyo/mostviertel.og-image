136921f7621632af924cdafe36fef8e73bf0327c
Alex Spieslechner <alexander.spieslechner@gmail.com>
2022-10-09 12:36:09 +0200

    move css out of handler

--- api/index.ts
+++ api/index.ts
@@ -10,6 +10,75 @@ const bold = readFileSync(
   `${__dirname}/fonts/PlusJakartaSans/PlusJakartaSans-Bold.woff2`
 ).toString("base64");
 
+const css = `
+  @font-face {
+    font-family: 'PlusJakartaSans';
+    font-style:  normal;
+    font-weight: normal;
+    src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
+  }
+  @font-face {
+      font-family: 'PlusJakartaSans';
+      font-style:  normal;
+      font-weight: bold;
+      src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
+  }
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    position: relative;
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: PlusJakartaSans, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+  .border {
+    position: absolute;
+    height: 10px;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: linear-gradient(to bottom right, #f97316, #f59e0b)
+  }
+  h1 {
+    line-height: 1;
+    margin: 0;
+  }
+  .excerpt {
+    max-width: 500px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    line-height: 1.6;
+    color: #666;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+  }
+  ul, li {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  .tags {
+    display: flex;
+    gap: 4px;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+  .tag {
+    background-color: rgb(243 244 246);
+    padding: 4px 8px;
+    border-radius: 4px;
+    font-size: 12px;
+    line-height: 1;
+    color: #666;
+  }
+`;
+
 function toArray(input: string[] | string): string[] {
   if (Array.isArray(input)) return input;
   return [...input];
@@ -27,75 +96,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     if (Array.isArray(excerpt)) {
       throw new Error("excerpt must be a single value");
     }
-
-    const css = `
-      @font-face {
-        font-family: 'PlusJakartaSans';
-        font-style:  normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
-      }
-      @font-face {
-          font-family: 'PlusJakartaSans';
-          font-style:  normal;
-          font-weight: bold;
-          src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
-      }
-      * {
-        box-sizing: border-box;
-      }
-      body {
-        position: relative;
-        margin: 0;
-        padding: 0;
-        height: 100vh;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-family: PlusJakartaSans, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-      }
-      .border {
-        position: absolute;
-        height: 10px;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        background: linear-gradient(to bottom right, #f97316, #f59e0b)
-      }
-      h1 {
-        line-height: 1;
-        margin: 0;
-      }
-      .excerpt {
-        max-width: 500px;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        line-height: 1.6;
-        color: #666;
-        display: -webkit-box;
-        -webkit-line-clamp: 3;
-        -webkit-box-orient: vertical;
-      }
-      ul, li {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-      }
-      .tags {
-        display: flex;
-        gap: 4px;
-        align-items: center;
-        flex-wrap: wrap;
-      }
-      .tag {
-        background-color: rgb(243 244 246);
-        padding: 4px 8px;
-        border-radius: 4px;
-        font-size: 12px;
-        line-height: 1;
-        color: #666;
-      }
-    `;
 
     const html = `<!DOCTYPE html>
       <html lang="en">
